09f8836fb183ceb8ca924aef0d7277964e94afd8
Nathan Woodruff <Nathan.Woodruff@inl.gov>
2025-04-19 20:06:03 -0500

    Update data.tsx

--- app/src/app/routes/data/data.tsx
+++ app/src/app/routes/data/data.tsx
@@ -7,13 +7,22 @@ import { PWM } from "./timeseries/charts/pwm/pwm";
 import { Impedance } from "./timeseries/charts/impedance/impedance";
 import { Potentiostatic } from "./timeseries/charts/potentiostatic/potentiostatic";
 
+// Types
+import { QueryResultT } from "@/lib/types/timeseries";
+
 export default function Faraday() {
+  const data: QueryResultT | undefined = useAppSelector(
+    (state) => state.warehouse.data
+  );
+
   return (
     <Visualizer>
       <div className="bg-base-100">
-        {/* <PWM />
-      <Impedance data={data} />
-      <Potentiostatic data={data} /> */}
+        {data ? (
+          data.type === "Impedance" ? (
+            <Impedance timeseries={data.timeseries} />
+          ) : null
+        ) : null}
       </div>
     </Visualizer>
   );
