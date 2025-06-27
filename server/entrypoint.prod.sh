d27b2b96c244a96b58ea758c4609b85e123e297f
Nathan Woodruff <Nathan.Woodruff@inl.gov>
2025-06-26 19:53:51 -0500

    fix port for /admin

--- server/entrypoint.prod.sh
+++ server/entrypoint.prod.sh
@@ -1,6 +1,3 @@
 #! /bin/bash
 
-python manage.py collectstatic --noinput --clear
-python manage.py makemigrations
-python manage.py migrate
-gunicorn --bind 0.0.0.0:8000 --timeout 900 --log-level info server.wsgi
+python manage.py collectstatic --noinput --clear && python manage.py makemigrations && python manage.py migrate && gunicorn --bind 0.0.0.0:8000 --timeout 900 --log-level info server.wsgi
